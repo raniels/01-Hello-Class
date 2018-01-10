09867c23e60520d9dfadfb6330df1f44773b26ab
raniels <35283647+raniels@users.noreply.github.com>
2018-01-10 14:54:20 -0500

    Answered question in 01 hello.py

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,8 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('Hello, Class!\nMy name is Raniel Santos (pronounced like Daniel but with an R instead of a D') # What is your name
+print('My favorite game is Brood War. Although it is not the game I have spent the most hours on, or a game I\'m good at,\nit\'s the first game I\'ve played and I love watching it\'s high inensity competitive matches.') # What is your favorite game?
+print('My main concern for this class is being unable to learn new concepts and ideas which may not be quite native to my thinking.') # What concerns do you have about this class?
+print('However, I am excited about learning how to program and finding different ways I can use what I learn in  life to make it easier.') # What are you excited about (in general)?
+print('My stackoverflow user number is 9196369 and my github url is https://github.com/raniels') # What is your stackoverflow.com user number and what is the url to your github profile?
